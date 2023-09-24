b3c6449779e59e61af32ff991eefd392fee7c70a
William H <william.hermozo@protonmail.com>
2023-09-24 20:23:48 +0100

    past page

---
+++ app/past/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <section className="flex flex-col gap-2 items-center">
+      <h1>past prompts</h1>
+    </section>
+  );
+}
--- app/submitted/page.tsx
+++ app/submitted/page.tsx
@@ -1,11 +1,18 @@
+import { CalendarIcon } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <section>
+    <section className="flex flex-col gap-12 items-center">
       <h1 className="text-center">
         submitted
         <br />
         <span className="text-muted-foreground">great reflection</span>
       </h1>
+
+      <Link href="/past">
+        <CalendarIcon className="w-4 h-4" />
+      </Link>
     </section>
   );
 }
